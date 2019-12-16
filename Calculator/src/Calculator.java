325a1a17fc44cc8d20653594c1f3b52afce73896
cristianabote <cristianamihaelaplesa@gmail.com>
2019-12-16 23:33:44 +0200

    version1

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
+public class Calculator
+{
+    public static void main ( String[] args)
+    {
+        int x=5,y=2;
+        char c='-';
+        System.out.println("citeste primul numar x="+x);
 
-public class Calculator {
+        System.out.println("citeste al doilea numar y="+y);
+
+        System.out.println("operatia c="+c);
+
+        switch(c)
+        {
+            case '+' : System.out.println("x+y="+(x+y));
+                break;
+            case '-' : System.out.println("x-y="+(x-y));
+                break;
+            case '*' : System.out.println("x*y="+ (x*y));
+                break;
+            case '/' : System.out.println("x/y="+(x/y));
+                break;
+            case '%' : System.out.println("x%y="+(x%y));
+                break;
+            default: System.out.println(c+" operator invalid");
+        }
+    }
 }
